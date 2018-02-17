495159f605734c150c446da5f6081e946ab0fc49
buraksofu <buraksofu77@gmail.com>
2018-02-17 03:07:19 +0300

    MongoDB deployed

--- server/config/config.js
+++ server/config/config.js
@@ -2,8 +2,8 @@ var env = process.env.NODE_ENV || "development";
 
 if (env === "development") {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = "mongodb://localhost:27017/isteApp";
+  // process.env.MONGODB_URI = "mongodb://localhost:27017/isteApp";
 } else if (env === "test") {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = "mongodb://localhost:27017/isteAppTest";
+  // process.env.MONGODB_URI = "mongodb://localhost:27017/isteAppTest";
 }
