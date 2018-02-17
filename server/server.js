6747a2032df892a3f2a981da673a6501f127095b
buraksofu <buraksofu77@gmail.com>
2018-02-17 13:41:03 +0300

    geocode post endpoint implemented

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require("./config/config");
 const _ = require("lodash");
 const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 const { ObjectID } = require("mongodb");
 
 var { mongoose } = require("./db/mongoose");
@@ -18,6 +19,28 @@ app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send('Getir Hackathon18 "aba" backend');
+});
+
+app.post("/location", (req, res) => {
+  // Should check if address is sent in request!
+  var encodedAddress = encodeURIComponent(req.body.address);
+  var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyCi5yPbz6YZ4EyrJsoKFZ4vaa-NBzqDc04`;
+  console.log(encodedAddress);
+  axios
+    .get(geocodeUrl)
+    .then(response => {
+      if (response.data.status === "ZERO_RESULTS") {
+        throw new Error("Unable to find that address.");
+      }
+
+      var lat = response.data.results[0].geometry.location.lat;
+      var lng = response.data.results[0].geometry.location.lng;
+      console.log(response.data.results[0].formatted_address);
+      res.send({ Lat: lat, Lng: lng });
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
 });
 
 app.get("/orders", (req, res) => {
