19763244c60bf8daebd49baeaa9585566758eda6
buraksofu <buraksofu77@gmail.com>
2018-02-18 04:23:40 +0300

    quick fix

--- helpers/available.js
+++ helpers/available.js
@@ -3,18 +3,10 @@ var Courier = require("../models/courier");
 var Order = require("../models/order");
 const { ObjectID } = require("mongodb");
 
-module.exports = (courier, weight, count) => {
-  return new Promise((resolve, reject) => {
-    if (!point.location) {
-      reject("Point should have location object!");
-    }
-    const result = couriers.filter(
-      courier =>
-        geodist(
-          { lat: point.location.lat, lng: point.location.lng },
-          { lat: courier.location.lat, lng: courier.location.lng }
-        ) <= range
-    );
-    resolve(result);
-  });
-};
+// module.exports = (courier, weight, count) => {
+//   return new Promise((resolve, reject) => {
+
+//     Order.find
+
+//   });
+// };
